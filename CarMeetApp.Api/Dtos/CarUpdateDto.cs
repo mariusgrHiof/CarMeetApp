f9a339170e7cdd0ff44e96041d9e7e2506ac4652
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2022-12-15 13:39:22 +0100

    Add CarUpdateDto

---
+++ CarMeetApp.Api/Dtos/CarUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CarMeetApp.Api.Dtos
+{
+    public class CarUpdateDto
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Hp { get; set; }
+    }
+}
